7ee587cb07d9584b33127491d7ac0fc233cbc3ea
liosixer-hub <360287333@qq.com>
2026-01-08 01:22:37 +0800

    Update rsbuild.config.ts

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
-import { pluginModuleFederation } from '@module-federation/rsbuild-plugin';
-import moduleFederationConfig from './module-federation.config';
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
+import { pluginModuleFederation } from "@module-federation/rsbuild-plugin";
+import moduleFederationConfig from "./module-federation.config";
 
-const publicUrl = process.env.PUBLIC_URL || 'http://localhost:3001/';
+const publicUrl = process.env.PUBLIC_URL || "http://localhost:3001/";
 
 export default defineConfig({
   plugins: [pluginReact(), pluginModuleFederation(moduleFederationConfig)],
@@ -14,11 +14,14 @@ export default defineConfig({
   dev: {
     assetPrefix: publicUrl,
   },
+  output: {
+    assetPrefix: publicUrl,
+  },
   tools: {
     rspack: {
       watchOptions: {
         ignored: /node_modules/,
-      }
-    }
-  }
+      },
+    },
+  },
 });
